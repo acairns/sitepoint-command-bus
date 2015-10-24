cbaa7f3b39f29552f212a789a2c80c1f10d6c3d0
Andrew Cairns <andrew@acairns.co.uk>
2015-10-24 20:22:00 +0100

    Example DrawCard Command

---
+++ examples/DrawCardExample.php
@@ -0,0 +1,26 @@
+<?php require_once __DIR__ . '/../bootstrap.php';
+
+use DeckOfCards\Domain\Deck;
+use DeckOfCards\Domain\DeckId;
+use DeckOfCards\Application\Commands\DrawCard;
+use DeckOfCards\Application\Commands\DrawCardHandler;
+use DeckOfCards\Infrastructure\Repositories\InMemoryDeckRepository;
+
+$decks = new InMemoryDeckRepository;
+
+$deckId = DeckId::generate();
+
+$decks->add(
+    Deck::standard($deckId)
+);
+
+$locator->addHandler(
+    new DrawCardHandler($decks),
+    DrawCard::class
+);
+
+$card = $bus->handle(
+    new DrawCard($deckId)
+);
+
+print_r($card);
